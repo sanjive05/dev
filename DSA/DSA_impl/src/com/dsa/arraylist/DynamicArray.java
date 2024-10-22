21a401dc06f4956ffee49caa395dc03bd85e625e
sanjive05 <sanjivesubramaniam4154@gmail.com>
2024-10-22 19:57:28 +0530

    first commit - hello world

---
+++ DSA/DSA_impl/src/com/dsa/arraylist/DynamicArray.java
@@ -0,0 +1,20 @@
+package com.dsa.arraylist;
+
+public class DynamicArray {
+
+    int[] arr;
+    int size;
+    int capacity;
+    static  final int intialCapacity = 16;
+
+    public DynamicArray(){
+        size=0;
+        arr=new int[16];
+        capacity = intialCapacity;
+
+    }
+
+    public void add(int val) {
+
+    }
+}
---
+++ DSA/DSA_impl/src/com/dsa/arraylist/Test.java
@@ -0,0 +1,13 @@
+package com.dsa.arraylist;
+
+public class Test {
+    public static void main(String[] args) {
+        DynamicArray array = new DynamicArray();
+        array.add(45);
+        array.display();
+        array.insertAtPos(45,2);
+        array.deleteAtPos(2);
+
+
+    }
+}
