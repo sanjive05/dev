18f2b1d015baf3e91dd72403c7426aa1f3061142
sanjive05 <sanjivesubramaniam4154@gmail.com>
2024-10-26 19:08:54 +0530

    linked list partially implemented

--- DSA/DSA_impl/src/com/dsa/linkedlist/LinkedList.java
+++ DSA/DSA_impl/src/com/dsa/linkedlist/LinkedList.java
@@ -1,4 +1,54 @@
 package com.dsa.linkedlist;
 
 public class LinkedList {
+    Node head;
+
+
+    class Node{
+        int data;
+        Node next;
+        public Node(int val){
+            this.data=val;
+        }
+    }
+    public LinkedList(){
+        head=null;
+    }
+    public void insertAtBegin(int val) {
+        Node newNode = new Node(val);
+        if(head==null){
+            head = newNode;
+        }
+        else{
+            newNode.next = head;
+            head= newNode;
+        }
+
+    }
+    public void insertAtPos(int pos, int val) {
+        Node newNode = new Node(val);
+        if(pos==0){
+            insertAtBegin(val);
+        }
+        Node temp = head;
+        for(int i=0;i<pos-1;i++){
+            temp=temp.next;
+            if(temp==null){
+                throw new ArrayIndexOutOfBoundsException("Invalid Position");
+            }
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+"  ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+
 }
--- DSA/DSA_impl/src/com/dsa/linkedlist/Test.java
+++ DSA/DSA_impl/src/com/dsa/linkedlist/Test.java
@@ -2,6 +2,13 @@ package com.dsa.linkedlist;
 
 public class Test {
     public static void main(String[] args) {
-        
+        LinkedList list = new LinkedList();
+        list.insertAtBegin(4);
+        list.insertAtBegin(5);
+        list.insertAtBegin(23);
+        list.insertAtPos(1,65);
+        list.insertAtPos(3,78);
+        list.insertAtPos(5,8);
+        list.display();
     }
 }
