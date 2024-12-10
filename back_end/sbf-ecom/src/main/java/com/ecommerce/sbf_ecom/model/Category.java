09fb912d119f62c4314ec1ab2d24d021c5a7626d
sanjive05 <sanjivesubramaniam4154@gmail.com>
2024-12-10 07:19:46 +0530

    working with repositories

--- back_end/sbf-ecom/src/main/java/com/ecommerce/sbf_ecom/model/Category.java
+++ back_end/sbf-ecom/src/main/java/com/ecommerce/sbf_ecom/model/Category.java
@@ -1,6 +1,11 @@
 package com.ecommerce.sbf_ecom.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "categories")
 public class Category {
+    @Id
     private long categoryId;
     private  String categoryName;
 
